88c684869b3b63fab357564232d1571507cba66a
Cristhian <cristhianbay@gmail.com>
2020-02-21 02:47:12 +0100

    Creada clase APIUtils de Retrofit

---
+++ app/src/main/java/com/crisgon/autocartasgui/retrofit2/APIUtils.java
@@ -0,0 +1,19 @@
+package com.crisgon.autocartasgui.retrofit2;
+
+import com.crisgon.autocartasgui.retrofit2.APIRestClient;
+import com.crisgon.autocartasgui.retrofit2.APIService;
+
+/**
+ * Created by @cristhian-jg on 18/02/2020.
+ */
+public class APIUtils {
+
+    private APIUtils() {
+    }
+
+    public static final String BASE_URL = "http://192.168.1.39:8080";
+
+    public static APIService getAPIService() {
+        return APIRestClient.getInstance(BASE_URL).getRetrofit().create(APIService.class);
+    }
+}
